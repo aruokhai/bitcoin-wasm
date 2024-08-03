77d9b4a574fdedc366bbdfb3ad7d618a6dffaf0d
Joshua Aruokhai <joshuaaruokhaitech@gmail.com>
2024-08-03 09:56:08 +0100

    started the implementation of wasi compliant tbdex

---
+++ crates/tbdex/src/dids/mod.rs
@@ -0,0 +1,7 @@
+pub mod data_model;
+pub mod methods;
+pub mod resolution;
+
+pub mod bearer_did;
+pub mod did;
+pub mod portable_did;
---
+++ crates/tbdex/src/lib.rs
@@ -0,0 +1,18 @@
+#[allow(warnings)]
+mod bindings;
+
+use bindings::Guest;
+
+mod  crypto;
+mod dids;
+mod request;
+struct Component;
+
+impl Guest for Component {
+    /// Say hello!
+    fn hello_world() -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
